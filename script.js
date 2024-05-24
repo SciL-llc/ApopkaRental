52b46211d17eaf3b5861676c6931d896846ea291
SciL-llc <167808098+SciL-llc@users.noreply.github.com>
2024-05-24 21:09:43 +0000

    pausing swipe til further notice

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let topSwiper = new Swiper(".swiper.is-top", {
-    effect: "fade",
-    fadeEffect: {
-      crossFade: true,
-      longSwipe: false,
-    },
-    loop: true,
-    followFinger: false,
-    navigation: {
-      nextEl:".arrow.is-right",
-      prevEl:".arrow.is-left"
-    }
-  });
+// let topSwiper = new Swiper(".swiper.is-top", {
+//     effect: "fade",
+//     fadeEffect: {
+//       crossFade: true,
+//       longSwipe: false,
+//     },
+//     loop: true,
+//     followFinger: false,
+//     navigation: {
+//       nextEl:".arrow.is-right",
+//       prevEl:".arrow.is-left"
+//     }
+//   });
   
-  let bottomSwiper = new Swiper(".swiper.bottom", {
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "2",
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
-  topSwiper.controller.control = bottomSwiper;
-  bottomSwiper.controller.control = topSwiper;
+//   let bottomSwiper = new Swiper(".swiper.bottom", {
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: "2",
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//   });
+//   topSwiper.controller.control = bottomSwiper;
+//   bottomSwiper.controller.control = topSwiper;
   
